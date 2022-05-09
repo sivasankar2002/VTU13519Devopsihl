166c9fac7ae73a6a2abdf3f6f8639368e1e74b28
SIVA SANKAR <vtu13519@veltech.edu.in>
2022-05-09 15:22:44 +0530

    first commit

---
+++ devops1.py
@@ -0,0 +1,16 @@
+import calculator
+
+
+class TestCalculator:
+
+    def test_addition(self):
+        assert 4 == calculator.add(2, 2)
+
+    def test_subtraction(self):
+        assert 2 == calculator.subtract(4, 2)
+
+    def test_multiplication(self):
+        assert 8 == calculator.multiply(4, 2)
+
+    def test_division(self):
+        assert 2 == calculator.division(4, 2)
